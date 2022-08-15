6447b89d74a13e1662acd56f671fad7793a687f0
Vicky Jones <vlj8008@gmail.com>
2022-08-15 10:04:19 -0500

    Scripts
    Complete LocatingCSharpFiles script.
    Deleting comments script still needing work.

---
+++ DeletingComments.ps1
@@ -0,0 +1,35 @@
+﻿
+param(
+    $path = "C:\test"
+)
+
+#Create an array of the c# files from User chosen directory.
+
+$csfiles = @(Get-ChildItem -Path $path -Exclude *aspx.designer.cs, *designer.cs -Filter *.cs   -Recurse)
+
+
+# Function to delete comment lines
+
+function DeleteCommentLines
+
+{
+    foreach($file in $csfiles)
+    {
+
+    # create array of comment lines. 
+
+    $lines = @(Get-Content -Path $file|Where-Object{$_ -match"//"})
+
+    # loop through comment lines and delete.
+     
+    foreach ($line in $lines)
+    {
+
+       $line
+       # need code here to delete the line. 
+        
+    }
+    
+    }
+}
+
